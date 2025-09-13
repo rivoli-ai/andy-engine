eaa1997e394ee09bc7c9a9a701be623fe94d6084
Sami Ben Grine <sami.bengrine@gmail.com>
2025-09-13 07:29:47 -0700

    Add all files

---
+++ src/Andy.Engine/EngineInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Andy.Engine;
+
+public static class EngineInfo
+{
+    public static string Name => "Andy.Engine";
+    public static string Version => typeof(EngineInfo).Assembly.GetName().Version?.ToString() ?? "0.0.0";
+}
